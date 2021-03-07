2c4997c1a38b1f2e4efb2ed368fa744cc7a23ef1
JBoyce298 <jboyce4@ggc.edu>
2021-03-07 15:27:29 -0500

    enemy movement adjustments. enemy death, walk, hurt animations adjusted

--- Assets/scripts/skeletoncontroller.cs
+++ Assets/scripts/skeletoncontroller.cs
@@ -7,66 +7,148 @@ public class skeletoncontroller : MonoBehaviour
     Animator myAnim;
     SpriteRenderer sr;
     Rigidbody2D myBod;
+    BoxCollider2D myBox;
 
     public bool hurt;
     public int health;
     public float speed;
+    public float distance;
+
+    private float walkDelay = 0;
+    private bool isRight = true;
+    private bool delay = false;
+    private float stopCheck;
+    private float stopTimer = 0;
+    private Vector2 tether;
+
+    private bool dead = false;
     // Start is called before the first frame update
     void Start()
     {
         myAnim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         myBod = GetComponent<Rigidbody2D>();
+        myBox = GetComponent<BoxCollider2D>();
+
+        tether = transform.position;
+        stopCheck = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!hurt)
+        if (!dead)
         {
-           /* float x = speed;
-            if(x > 0)
+            if (!hurt)
             {
-                sr.flipX = false;
-                myAnim.SetBool("WALK", true);
+                //movement
+                if (!delay)
+                {
+                    if (isRight)//walking right
+                    {
+                        sr.flipX = false;
+                        myAnim.SetBool("WALK", true);
+                        if (transform.position.x - tether.x > distance)
+                        {
+                            delay = true;
+                            myBod.velocity = new Vector2(0, myBod.velocity.y);
+                        }
+                        else
+                        {
+                            myBod.velocity = new Vector2(speed, myBod.velocity.y);
+                        }
+                    }
+                    else//walking left
+                    {
+                        sr.flipX = true;
+                        myAnim.SetBool("WALK", true);
+
+                        if (transform.position.x - tether.x < (distance * -1))
+                        {
+                            delay = true;
+                            myBod.velocity = new Vector2(0, myBod.velocity.y);
+                        }
+                        else
+                        {
+                            myBod.velocity = new Vector2((speed * -1), myBod.velocity.y);
+                        }
+                    }
+
+                    stopTimer += Time.deltaTime;
+                    if (stopTimer > 0.2)
+                    {
+                        stopTimer = 0;
+                        if (stopCheck == transform.position.x)
+                        {
+                            delay = true;
+                        }
+                        stopCheck = transform.position.x;
+                    }
+                }
+                else
+                {
+                    myAnim.SetBool("WALK", false);
+                    walkDelay += Time.deltaTime;
+
+                    if(walkDelay >= 2.5)
+                    {
+                        isRight = !isRight;
+                        delay = false;
+                        walkDelay = 0;
+                    }
+                }
+
+                //dying
+                if (health <= 0)
+                {
+                    myAnim.SetBool("DIE", true);
+                    dead = true;
+                }
             }
-            else if (x < 0)
+
+            //getting hit
+            if (hurt)
             {
-                sr.flipX = true;
-                myAnim.SetBool("WALk", true);
+                if(myAnim.GetBool("HURT") == false)
+                {
+                    health--;
+                }
+
+                myAnim.SetBool("HURT", true);
             }
             else
             {
-                myAnim.SetBool("WALk", false);
-            }*/
+                myAnim.SetBool("HURT", false);
+            }
         }
-
-        if(hurt)
+        else
         {
-            myAnim.SetBool("HURT", true);
-            health--;
-        }
-        else 
-        {
-            myAnim.SetBool("HURT", false);
+            myBox.size = new Vector2(0.721f,0.310f);
+            myBox.offset = new Vector2(0.161f,-0.7f);
         }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject g = collision.gameObject;
-        if(collision.tag == "Player")
+        if(g.tag == "Player")
         {
-            g.GetComponent<playercontroller>().isHurt = true;
+            if (!dead)
+            {
+                g.GetComponent<playercontroller>().isHurt = true;
+            }  
         }
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
         GameObject g = collision.gameObject;
-        if (collision.tag == "Player")
+        if (g.tag == "Player")
         {
-            g.GetComponent<playercontroller>().isHurt = false;
+            if (!dead)
+            {
+                g.GetComponent<playercontroller>().isHurt = false;
+            }
         }
     }
 }
